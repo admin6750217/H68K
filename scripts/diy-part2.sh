f8d08e67a781ae0a7d80345ea76f86703485276b
admin6750217 <58500194+admin6750217@users.noreply.github.com>
2026-09-08 03:01:19 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -4,10 +4,11 @@ set -e
 cd /workdir/openwrt
 
 echo "===== DIY Part2 ====="
-echo "编译前配置处理完成"
 
+# =========================================================
+# 删除 feeds 中与第三方插件冲突的版本
+# =========================================================
 
-# 删除feeds中的插件
 rm -rf ./feeds/packages/net/{geoview,chinadns-ng,hysteria,mosdns,v2ray-geodata,lucky}
 rm -rf ./feeds/packages/net/{shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev}
 rm -rf ./feeds/packages/net/{sing-box,v2ray-geodata,v2ray-plugin,xray-core,smartdns}
@@ -16,69 +17,127 @@ rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-passwall2,luci-app-
 rm -rf ./feeds/luci/applications/{luci-app-lucky,luci-app-smartdns,luci-app-timecontrol,luci-app-mosdns}
 rm -rf ./feeds/luci/applications/{luci-app-nikki,luci-app-momo,luci-app-daed}
 
-# 克隆依赖插件
-git clone --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git package/pwpage
-# git clone --depth 1 https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+# =========================================================
+# PassWall 依赖
+# =========================================================
 
+rm -rf package/pwpage
 
-# 克隆的源码放在small文件夹
-mkdir package/small
+git clone \
+  --depth 1 \
+  https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git \
+  package/pwpage
+
+# =========================================================
+# 第三方插件
+# =========================================================
+
+mkdir -p package/small
 pushd package/small
 
-# luci-theme-aurora
-git clone -b master --depth 1 https://github.com/eamonxg/luci-theme-aurora.git
+# Aurora 主题
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/eamonxg/luci-theme-aurora.git
 
-# luci-app-nft-timecontrol
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
+# TimeControl
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/sirpdboy/luci-app-timecontrol.git
 
-# adguardhome
-# git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+# HomeProxy
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/immortalwrt/homeproxy.git
 
-# homeproxy
-git clone -b master --depth 1 https://github.com/immortalwrt/homeproxy.git
+# Lucky
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/gdy666/luci-app-lucky.git
 
-# lucky
-git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
+# SmartDNS LuCI
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/pymumu/luci-app-smartdns.git
 
-# smartdns
-git clone -b master --depth 1 https://github.com/pymumu/luci-app-smartdns.git
-git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
-sed -i 's@include ../../lang/rust/rust-package.mk@include $(TOPDIR)/feeds/packages/lang/rust/rust-package.mk@g' smartdns/package/openwrt/Makefile
+# SmartDNS
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/pymumu/smartdns.git
+
+sed -i \
+  's@include ../../lang/rust/rust-package.mk@include $(TOPDIR)/feeds/packages/lang/rust/rust-package.mk@g' \
+  smartdns/package/openwrt/Makefile
+
+echo "===== SmartDNS Rust Makefile ====="
 sed -n '33p' smartdns/package/openwrt/Makefile
 
-# ssrp
-# git clone -b master --depth 1 https://github.com/fw876/helloworld.git
+# VIKINGYFY packages
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/VIKINGYFY/packages.git
 
-# VIKINGYFY/packages
-git clone -b main --depth 1 https://github.com/VIKINGYFY/packages.git
+# PassWall
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/Openwrt-Passwall/openwrt-passwall.git
 
-# passwall
-git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall.git
+# PassWall2
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/Openwrt-Passwall/openwrt-passwall2.git
 
-# passwall2
-git clone -b main --depth 1 https://github.com/Openwrt-Passwall/openwrt-passwall2.git
+# MosDNS
+git clone \
+  -b v5 \
+  --depth 1 \
+  https://github.com/sbwml/luci-app-mosdns.git
 
-# mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# =========================================================
+# Netspeedtest
+# 暂时禁用：当前仓库 clone 时出现 GitHub 认证错误
+# =========================================================
 
-# luci-app-netspeedtest
-git clone -b master --depth 1 https://github.com/sirpdboy/luci-app-netspeedtest.git
+# git clone \
+#   -b master \
+#   --depth 1 \
+#   https://github.com/sirpdboy/luci-app-netspeedtest.git
 
-# openclash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
+# OpenClash
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/vernesong/OpenClash.git
 
-# OpenWrt-nikki
-git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+# Nikki
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
-# OpenWrt-momo
-git clone -b main --depth 1 https://github.com/nikkinikki-org/OpenWrt-momo.git
+# Momo
+git clone \
+  -b main \
+  --depth 1 \
+  https://github.com/nikkinikki-org/OpenWrt-momo.git
 
-# daed
-git clone -b master --depth 1 https://github.com/QiuSimons/luci-app-daed.git
-
-#modem
-# git clone -b main --depth 1 https://github.com/FUjr/modem_feeds.git
+# Daed
+git clone \
+  -b master \
+  --depth 1 \
+  https://github.com/QiuSimons/luci-app-daed.git
 
 popd
 
-echo "packages executed successfully!"
+echo "========================================"
+echo "DIY Part2 插件源码下载完成"
+echo "========================================"
